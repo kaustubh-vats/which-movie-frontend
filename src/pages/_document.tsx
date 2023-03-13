d9954588b60e5c07b2510c668939005de5527c70
Kaustubh Vats <kaustubhvats08@gmail.com>
2023-03-13 20:28:20 +0530

    Initial commit

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Which Movie</title>
+        <script src="https://kit.fontawesome.com/8976ccf748.js" crossOrigin="anonymous"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
